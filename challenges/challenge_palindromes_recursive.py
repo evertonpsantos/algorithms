3bee16d71361f5208e68d6b97d17d0554cdc75f1
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-26 19:21:44 -0300

    feat: adds is_palindrome_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '' or word is None:
+        return False
+
+    if high_index <= low_index:
+        return True
+
+    if word[high_index] != word[low_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
