59b467f964b54542192dbacabbc1cbddf2560e25
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-26 17:02:27 -0300

    feat: adds study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        student_count = 0
+
+        for entry_time, exit_time in permanence_period:
+            if entry_time <= target_time <= exit_time:
+                student_count += 1
+
+        return student_count
+
+    except TypeError:
+        return None
