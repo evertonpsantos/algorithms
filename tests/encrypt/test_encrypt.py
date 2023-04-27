b9951f16d9e106beea567e8321776d3290b8b26e
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-26 21:44:07 -0300

    feat: adds test to encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("python", 2) == 'noht_yp'
+    assert encrypt_message("computacional", 3) == 'moc_lanoicatup'
+    assert encrypt_message("algorítmo", 22) == 'omtírogla'
+
+    invalid_message_error = 'tipo inválido para message'
+    invalid_key_error = 'tipo inválido para key'
+
+    with pytest.raises(TypeError, match=invalid_message_error):
+        encrypt_message(3, 2)
+
+    with pytest.raises(TypeError, match=invalid_key_error):
+        encrypt_message('abracaba', 'dois')
